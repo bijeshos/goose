91517c624eee0553cc413fbebb99165aef848b14
Bijesh O S <bijeshos@users.noreply.github.com>
2020-03-01 19:15:26 +0530

    basic version of fileutil

--- io/fileutil/fileutil.go
+++ io/fileutil/fileutil.go
@@ -2,9 +2,27 @@ package fileutil
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
 //Read to read from dir
-func Read() {
-	fmt.Println("inside fileutil:read")
+func Read(srcDir string) {
+	fmt.Println("reading files from: ", srcDir)
+
+	var files []string
+
+	err := filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
+		fmt.Println("----")
+		fmt.Println("path", path)
+		fmt.Println("file-info", info)
+		files = append(files, path)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		fmt.Println(file)
+	}
 }
