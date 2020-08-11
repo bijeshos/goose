46724ace92d8552eff75ee1aa617b2c286fd62f4
Bijesh O S <bijesh.os@gmail.com>
2020-08-11 16:07:18 +0530

    added function to create sub directories

--- io/dirutil/dirutil.go
+++ io/dirutil/dirutil.go
@@ -2,9 +2,18 @@ package dirutil
 
 import (
 	"fmt"
+	"os"
 )
 
 //Read to read from dir
 func Read() {
 	fmt.Println("inside dirutil:read")
+}
+
+//MkDirAll to create directories
+func MkDirAll(targetDir string) {
+	if _, err := os.Stat(targetDir); os.IsNotExist(err) {
+		fmt.Println("creating directory: ", targetDir)
+		os.MkdirAll(targetDir, os.ModePerm)
+	}
 }
