1c148fc8002141d6d9f517198b277dcb81421c81
Bijesh O S <bijesh.os@gmail.com>
2020-08-12 07:07:19 +0530

    moved read function to dirutil

--- dirutil/dirutil.go
+++ dirutil/dirutil.go
@@ -3,11 +3,23 @@ package dirutil
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 //Read to read from dir
-func Read() {
-	fmt.Println("inside dirutil:read")
+func Read(srcDir string) []string {
+	fmt.Println("reading files from: ", srcDir)
+	var files []string
+	err := filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
 
 //MkDirAll to create directories
--- fileutil/fileutil.go
+++ fileutil/fileutil.go
@@ -1,29 +1,12 @@
 package fileutil
 
 import (
-	"fmt"
 	"github.com/bijeshos/goose/dirutil"
 	"io"
 	"log"
 	"os"
 	"path/filepath"
 )
-
-//Read to read from dir
-func Read(srcDir string) []string {
-	fmt.Println("reading files from: ", srcDir)
-	var files []string
-	err := filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() {
-			files = append(files, path)
-		}
-		return nil
-	})
-	if err != nil {
-		panic(err)
-	}
-	return files
-}
 
 func CopyFile(srcPath, targetPath string) {
 	//open source file
