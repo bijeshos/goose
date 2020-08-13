88268a80826876e2b2c14e298cfd8ed0c0d30409
Bijesh O S <bijesh.os@gmail.com>
2020-08-13 10:47:54 +0530

    modified log statements

--- filesync/sync_init.go
+++ filesync/sync_init.go
@@ -1,44 +1,44 @@
 package filesync
 
 import (
-	"fmt"
 	"github.com/bijeshos/goose/dirutil"
 	"github.com/bijeshos/goose/fileutil"
+	"github.com/bijeshos/goose/logwrap"
 	"path/filepath"
 	"strings"
 )
 
 func Perform(srcDir string, targetDir string) {
-
+	logwrap.Infow("initiating sync")
 	dirExists, err := dirutil.IsExist(srcDir)
 	if err != nil {
-		fmt.Println("error occurred: ", err)
+		logwrap.Fatalw("error occurred: ", "error", err)
 		return
 	}
 	if !dirExists {
-		fmt.Println(srcDir, "does not exist. exiting program.")
+		logwrap.Fatalw("dir does not exist", "dir", srcDir)
 		return
 	}
 
 	dirSame, err := dirutil.IsSame(srcDir, targetDir)
 	if err != nil {
-		fmt.Println("error occurred: ", err)
+		logwrap.Fatalw("error occurred: ", "error", err)
 		return
 	}
 	if dirSame {
-		fmt.Println("both source and target directories are same. exiting program.")
+		logwrap.Fatalw("both source and target directories are same. exiting program.")
 		return
 	}
 
 	files := dirutil.Read(srcDir)
 	for _, file := range files {
-		fmt.Println("copying: from: ", file)
-		fmt.Println("copying: to  : ", filepath.Join(targetDir, filepath.Base(file)))
+		logwrap.Infow("copying", "from", file)
+		logwrap.Infow("copying", "to", filepath.Join(targetDir, filepath.Base(file)))
 		relativePath := strings.Replace(file, srcDir, "", 1)
 		targetPath := filepath.Join(targetDir, relativePath)
 		fileutil.CopyFile(file, targetPath)
-		fmt.Println("copy completed")
-		fmt.Println("------")
+		logwrap.Infow("copy completed")
+		logwrap.Infow("------")
 	}
 
 }
