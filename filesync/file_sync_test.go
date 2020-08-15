86c7569c26e5c5764e81f0b8b6dd196a206fe2ba
Bijesh O S <bijesh.os@gmail.com>
2020-08-15 06:28:08 +0530

    renamed file; modified logging

---
+++ filesync/file_sync_test.go
@@ -0,0 +1,22 @@
+package filesync
+
+import (
+	"fmt"
+	"github.com/bijeshos/goose/logwrap"
+	"os"
+	"testing"
+)
+
+func TestRead(t *testing.T) {
+
+	fmt.Println(os.Getwd())
+
+	logwrap.InitZapLogger("./.logs", "goose.log")
+
+	srcDir := "/home/bos/1-bos/tmp/test/goose/src-1"
+	targetDir := "/home/bos/1-bos/tmp/test/goose/target-1"
+	//targetDir := "/home/bos/1-bos/tmp/test/goose/src-1"
+
+	Perform(srcDir, targetDir)
+
+}
