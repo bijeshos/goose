e4dbe452e4b1b379c0d317947cd03497bfab1eeb
Bijesh O S <bijesh.os@gmail.com>
2020-08-21 22:50:09 +0530

    added config parameter retrieval

--- dirsync/dir_sync.go
+++ dirsync/dir_sync.go
@@ -3,6 +3,7 @@ package dirsync
 import (
 	"github.com/bijeshos/goose/dirutil"
 	"github.com/bijeshos/goose/fileutil"
+	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"path/filepath"
 	"strings"
@@ -10,11 +11,9 @@ import (
 
 func Perform(srcDir string, targetDir string) {
 	zap.S().Infow("initiating directory sync", "source", srcDir, "target", targetDir)
-
 	checkSrcDirExists(srcDir)
 	checkSrcAndTargetDirDiffer(srcDir, targetDir)
 	copyFiles(srcDir, targetDir)
-
 }
 
 func checkSrcDirExists(srcDir string) {
@@ -40,9 +39,9 @@ func checkSrcAndTargetDirDiffer(srcDir string, targetDir string) {
 }
 
 func copyFiles(srcDir string, targetDir string) {
-	files := dirutil.Read(srcDir)
+	ignoreList := fileutil.ReadFile(viper.GetString("sync.ignore-file-list"))
+	files := dirutil.Read(srcDir, ignoreList)
 	for _, file := range files {
-
 		relativePath := strings.Replace(file, srcDir, "", 1)
 		targetPath := filepath.Join(targetDir, relativePath)
 		fileutil.CopyFile(file, targetPath, false)
